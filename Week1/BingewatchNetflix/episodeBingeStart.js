a6f320a6b122cf662471ce7a3b26bc315e78ef07
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MBP.home>
2017-11-07 11:15:57 -0500

    Fixed bingeWatch

--- Week1/BingewatchNetflix/episodeBingeStart.js
+++ Week1/BingewatchNetflix/episodeBingeStart.js
@@ -1,5 +1,4 @@
 // The following are episodes for Arrested Development.
-
 const episodes = [
   { id: 's06e01', title: 'Pilot' },
   { id: 's06e02', title: 'Top Banana' },
@@ -14,19 +13,42 @@ const episodes = [
 
 
 const finaleEpisode = { id: 's06e10', title: 'Bringing Up Buster' };
-//Creating an empty array to add all the episodes of the season
 let playlist = [];
+let thisEp = 0;
 
  // Add this finaleEpisode to the episodes object and save that into addToPlaylist.
- //Taking the currentPlaylist and adding the finaleEpisode episode to it
 const addToPlaylist = (currentPlaylist, addEpisode) => {playlist = currentPlaylist.concat(addEpisode);}
 
 // You may want to add a function which takes away the current episode you were just watching and returns the new object consisting only of the
 // rest of the episodes which you have not viewed yet.
-//Take the first element of the episodes array and take out the episode we just watched
+
 const removeFromPlaylist = (justWatched) => justWatched.shift();
 
 // You might want to create a function which shows the viewer the next episode.
 // ** this would be a useful function after you have filtered or sliced the previous episode object out of the array **
-//Getting the next episode in the array 
 const getNextEpisodeInPlaylist = (currentPlaylist) => console.log(currentPlaylist[1]);
+
+function watchingNow (){
+    document.getElementById('watchNow').innerHTML = 'Watching Now: ' + playlist[0].title;
+    document.getElementById('comingNext').innerHTML = 'Coming Next: ' + playlist[1].title;
+}
+
+function previousEpisode(){
+    if(thisEp > 0){
+        thisEp -= 1;
+        document.getElementById('watchNow').innerHTML = 'Watching Now: ' + playlist[thisEp].title;
+        document.getElementById('comingNext').innerHTML = 'Coming Next: ' + playlist[thisEp + 1].title;
+    }
+}
+
+function nextEpisode(){
+    if(thisEp < playlist.length - 2){
+        thisEp += 1;
+        document.getElementById('watchNow').innerHTML = 'Watching Now: ' + playlist[thisEp].title;
+        document.getElementById('comingNext').innerHTML = 'Coming Next: ' + playlist[thisEp + 1].title;
+    }else if (thisEp < playlist.length - 1) {
+        thisEp += 1;
+        document.getElementById('watchNow').innerHTML = 'Watching Now: ' + playlist[thisEp].title;
+        document.getElementById('comingNext').innerHTML = 'Coming Next: Next Season';
+    }
+}
